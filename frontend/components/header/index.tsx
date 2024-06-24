8a00cdd005d325a9a4edee6900b35e18968d3b94
Max <mmandia511@gmail.com>
2024-06-24 11:53:56 -0400

    feat: making logo navigate to '/dashboard'

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AnteriorIcon from "@/public/anterior.svg";
+import Link from "next/link";
 
 function Header() {
     return (
         <div className="p-6 md:p-10">
-            <AnteriorIcon />
+            <Link href="/dashboard">
+                <AnteriorIcon />
+            </Link>
         </div>
     );
 }
