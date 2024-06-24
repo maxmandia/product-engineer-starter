e71718e5fde6f4f0942f9a20723dec86c372bfff
Max <mmandia511@gmail.com>
2024-06-24 09:48:37 -0400

    fix: making sure steps title is present when loading

--- frontend/components/steps/steps-container/index.tsx
+++ frontend/components/steps/steps-container/index.tsx
@@ -4,17 +4,17 @@ import StepComponent from "../step";
 import { Skeleton } from "@/components/ui/skeleton";
 
 function StepsContainer({ steps }: { steps: Step[] }) {
-    if (steps.length === 0) {
-        return <Skeleton className="h-[500px] w-full" />;
-    }
-
     return (
         <div className="flex flex-col gap-2 mb-10">
             <h4 className="text-lg font-bold">Steps</h4>
             <div className="flex flex-col gap-2 md:gap-10">
-                {steps.map((step, index) => (
-                    <StepComponent key={step.key} step={step} oneBasedIndex={index + 1} />
-                ))}
+                {steps.length === 0 ? (
+                    <Skeleton className="h-[500px] w-full" />
+                ) : (
+                    steps.map((step, index) => (
+                        <StepComponent key={step.key} step={step} oneBasedIndex={index + 1} />
+                    ))
+                )}
             </div>
         </div>
     );
