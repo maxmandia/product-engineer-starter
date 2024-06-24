419677fc1b08f79dae66d59905fc2e0f29e7df3f
Max <mmandia511@gmail.com>
2024-06-24 09:19:54 -0400

    style: adjusting desktop gap for steps

--- frontend/components/steps/steps-container/index.tsx
+++ frontend/components/steps/steps-container/index.tsx
@@ -11,9 +11,11 @@ function StepsContainer({ steps }: { steps: Step[] }) {
     return (
         <div className="flex flex-col gap-2 mb-10">
             <h4 className="text-lg font-bold">Steps</h4>
-            {steps.map((step, index) => (
-                <StepComponent key={step.key} step={step} oneBasedIndex={index + 1} />
-            ))}
+            <div className="flex flex-col gap-2 md:gap-10">
+                {steps.map((step, index) => (
+                    <StepComponent key={step.key} step={step} oneBasedIndex={index + 1} />
+                ))}
+            </div>
         </div>
     );
 }
