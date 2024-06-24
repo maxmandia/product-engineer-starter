51e94010bf44aa5c2b07c1513db6d712d3e64687
Max <mmandia511@gmail.com>
2024-06-24 09:08:29 -0400

    style: adding desktop padding for header

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -3,7 +3,7 @@ import AnteriorIcon from "@/public/anterior.svg";
 
 function Header() {
     return (
-        <div className="p-6">
+        <div className="p-6 md:p-10">
             <AnteriorIcon />
         </div>
     );
